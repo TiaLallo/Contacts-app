92a3fde0e7c7ed0ee874ac29b72dea017dc9a00c
TiaLallo <tia.lallo@gmail.com>
2019-03-19 11:07:11 +0200

    Initialized and database connection added

---
+++ webapi/ContactsWebApi/ContactsWebApi/Controllers/ContactsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ContactsWebApi.Models;
+using ContactsWebApi.Services;
+
+namespace ContactsWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Contact>> Get()
+        {
+            return new JsonResult(_contactService.Read());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Contact> Get(int id)
+        {
+            return new JsonResult(_contactService.Read(id));
+        }
+
+        [HttpPost]
+        public ActionResult<Contact> Create(Contact contact)
+        {
+            return _contactService.CreateContact(contact);
+        }
+
+        [HttpPut]
+        public ActionResult<Contact> Update(int id, Contact contact)
+        {
+            return _contactService.Update(id, contact);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _contactService.Delete(id);
+            return new NoContentResult();
+        }
+    }
+}
+}
---
+++ webapi/ContactsWebApi/ContactsWebApi/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactsWebApi.Models
+{
+    public partial class Contact
+    {
+        public long Id { get; set; }
+        [StringLength(10)]
+        public string FirstName { get; set; }
+        [StringLength(10)]
+        public string LastName { get; set; }
+        [StringLength(10)]
+        public string City { get; set; }
+        [StringLength(10)]
+        public string Address { get; set; }
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+        [StringLength(10)]
+        public string Telephone { get; set; }
+        [StringLength(10)]
+        public string Email { get; set; }
+    }
+}
