f30acc0c535d0a774ead2a0f8df2d3f746289087
TiaLallo <tia.lallo@gmail.com>
2019-03-19 11:07:39 +0200

    added repos and services

---
+++ webapi/ContactsWebApi/ContactsWebApi/Repositories/ContactRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactsWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsWebApi.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private ContactdbContext _context;
+
+        public ContactRepository(ContactdbContext context)
+        {
+            _context = context;
+        }
+
+        public Contact CreateContact(Contact contact)
+        {
+            _context.Add(contact);
+            _context.SaveChanges();
+            return contact;
+        }
+
+        public void Delete(int id)
+        {
+            var contact = Read(id);
+            _context.Remove(contact);
+            _context.SaveChanges();
+        }
+
+        public List<Contact> Read()
+        {
+            return _context.Contact.AsNoTracking().ToList();
+        }
+
+        public Contact Read(int id)
+        {
+            return _context.Contact.AsNoTracking().FirstOrDefault(c => c.Id == id);
+        }
+
+        public Contact Update(int id, Contact contact)
+        {
+            _context.Update(contact);
+            _context.SaveChanges();
+            return contact;
+        }
+    }
+}
---
+++ webapi/ContactsWebApi/ContactsWebApi/Services/ContactService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactsWebApi.Models;
+using ContactsWebApi.Repositories;
+
+namespace ContactsWebApi.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly IContactRepository _contactRepository;
+
+        public ContactService(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
+
+        public Contact CreateContact(Contact contact)
+        {
+            return _contactRepository.CreateContact(contact);
+        }
+
+        public void Delete(int id)
+        {
+            _contactRepository.Delete(id);
+        }
+
+        public List<Contact> Read()
+        {
+            return _contactRepository.Read();
+        }
+
+        public Contact Read(int id)
+        {
+            return _contactRepository.Read(id);
+        }
+
+        public Contact Update(int id, Contact contact)
+        {
+            return _contactRepository.Update(id, contact);
+        }
+    }
+}
